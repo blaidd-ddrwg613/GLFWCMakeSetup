61502b3d1931e28080f733b439e4313ea8985813
Blaidd_ddrwg613 <danielleger2011@gmail.com>
2025-01-01 21:30:23 -0500

    Happy Face

--- src/main.cpp
+++ src/main.cpp
@@ -65,7 +65,7 @@ int main()
             0.0f,0.5f,0.0f,0.0f, 0.0f, 1.0f,0.5f, 1.0f     // Top
             };
 
-    unsigned int texture;
+    unsigned int texture, texture2;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
 // set the texture wrapping/filtering options (on the currently bound texture object)
@@ -86,6 +86,29 @@ int main()
         std::cout << "Failed to load texture" << std::endl;
     }
     stbi_image_free(data);
+
+    // Texture 2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+// set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+    
+    data = stbi_load(RESOURCES_PATH"textures/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load second image" << "\n";
+    }
+    stbi_set_flip_vertically_on_load(true);
 
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -128,12 +151,16 @@ int main()
 
     // Shaders
     Shader shaderProgram(RESOURCES_PATH"shaders/default.vert",RESOURCES_PATH"shaders/default.frag");
+
+    shaderProgram.use();
+
+    glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture1"), 0); // set it manually
+    shaderProgram.setInt("texture2", 1); // or with shader class
+
     // render loop
-    // -----------
     while (!glfwWindowShouldClose(window))
     {
         // input
-        // -----
         processInput(window);
 
         // render
@@ -141,10 +168,11 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
 
-        // draw our first triangle
-        shaderProgram.use();
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 6);
